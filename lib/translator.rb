40367b324450f77e6a534d2bccbf4662be3ccbd1
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-08-15 00:38:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,9 +19,14 @@ end
 def get_japanese_emoticon(yamldata, emoticon)
  emoji_hash= load_library(yamldata)
  emoji_match = emoji_hash[:get_emoticon][emoticon]
-if emoji_match return emoji_match else return "Sorry, that emoticon was not found"
+  
+ if emoji_match
+   return emoji_match
+ else
+   return "Sorry, that emoticon was not found"
+ end
+  
 end
-
 def get_english_meaning
   # code goes here
 end
