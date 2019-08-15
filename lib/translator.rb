b7f1b8f690f7bcd5e8949fecde214754b2f1a83f
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-08-15 00:28:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,28 @@
 # require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(yamldata)
+  emoji_data = YAML.load_file('./lib/emoticons.yml')
+
+  emoji_hash = {get_meaning: {},
+                  get_emoticon: {} }
+  emoji_data.each do |meaning, emoji|
+    english = emoji[0]
+    japanese = emoji[1]
+    emoji_hash[:get_meaning][japanese] = meaning
+    emoji_hash[:get_emoticon][english] = japanese
+  end
+  emoji_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+def get_japanese_emoticon(yamldata, emoticon)
+ emoji_hash= load_library(yamldata)
+ emoji_match = emoji_hash[:get_emoticon][emoticon]
+if emoji_match == nil return "Sorry, that emoticon was not found"
+ else emoji_match
+end
+end
 end
 
 def get_english_meaning
